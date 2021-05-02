5a480a7d89dc7fdfedd337a32078a15077ea00fc
Prajwal Mani <prajwalmani@users.noreply.github.com>
2021-05-02 13:34:07 +0530

    tab menu

---
+++ src/main/andriod src/SLI/app/src/main/java/com/example/sli/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.sli.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.sli.Fragment1;
+import com.example.sli.Fragment2;
+import com.example.sli.Fragment3;
+import com.example.sli.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2,R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment=null;
+        switch (position){
+            case 0:
+                fragment=new Fragment1();
+                break;
+            case 1:
+                fragment= new Fragment2();
+                break;
+            case 2:
+                fragment=new Fragment3();
+                break;
+        }
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
