c127b0cda0e2457cb895b5eece06edf7d435faef
Prajwal Mani <prajwalmani@users.noreply.github.com>
2021-06-09 01:07:31 +0530

    camera added

--- src/SLI/app/src/main/java/com/example/sli/predictactivity.java
+++ src/SLI/app/src/main/java/com/example/sli/predictactivity.java
@@ -1,14 +1,61 @@
 package com.example.sli;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class predictactivity extends AppCompatActivity {
+    Button camerabtn;
+    ImageView imageView;
+    static final int CAM_REQUEST=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_predictactivity);
+        camerabtn=(Button)findViewById(R.id.camerabtn);
+        imageView=(ImageView)findViewById(R.id.imageView);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
+        camerabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera_intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getfile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+
+            }
+        });
+
+    }
+    private File getfile(){
+        File folder=new File("sliapp");
+        if(!folder.exists())
+        {
+            folder.mkdir();
+        }
+        File image_file = new File(folder,"campic.jpg");
+        return image_file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+//        super.onActivityResult(requestCode, resultCode, data);
+        String path = "sliapp/campic.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
     }
 }
