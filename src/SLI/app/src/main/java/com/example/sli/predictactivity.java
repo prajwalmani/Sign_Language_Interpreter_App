b9ec67fc0f48edc38253c11c59c517675e9b995f
Prajwal Mani <prajwalmani@users.noreply.github.com>
2021-06-09 19:23:20 +0530

    camera storage changed

--- src/SLI/app/src/main/java/com/example/sli/predictactivity.java
+++ src/SLI/app/src/main/java/com/example/sli/predictactivity.java
@@ -2,8 +2,14 @@ package com.example.sli;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,36 +32,27 @@ public class predictactivity extends AppCompatActivity {
         setContentView(R.layout.activity_predictactivity);
         camerabtn=(Button)findViewById(R.id.camerabtn);
         imageView=(ImageView)findViewById(R.id.imageView);
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-        StrictMode.setVmPolicy(builder.build());
-        builder.detectFileUriExposure();
+        if (ContextCompat.checkSelfPermission(predictactivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(predictactivity.this,new String[]{
+                    Manifest.permission.CAMERA},
+                    100);
+
+        }
         camerabtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent camera_intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = getfile();
-                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-                startActivityForResult(camera_intent,CAM_REQUEST);
-
+                Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
             }
         });
-
-    }
-    private File getfile(){
-        File folder=new File("sliapp");
-        if(!folder.exists())
-        {
-            folder.mkdir();
-        }
-        File image_file = new File(folder,"campic.jpg");
-        return image_file;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-//        super.onActivityResult(requestCode, resultCode, data);
-        String path = "sliapp/campic.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureimage = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(captureimage);
+        }
     }
 }
