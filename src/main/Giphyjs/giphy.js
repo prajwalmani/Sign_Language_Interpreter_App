67f76283feae956e4bccbe8bbe15e79de3cc1bf9
Prajwal Mani <56246392+prajwalmani@users.noreply.github.com>
2021-06-22 22:55:08 +0530

    api test commit

--- src/main/Giphyjs/giphy.js
+++ src/main/Giphyjs/giphy.js
@@ -1,15 +1,29 @@
-const form = document.forms[0];
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+async function spellcheck(word){
+    let mainapiurl="https://apimain.prajwalmani.repl.co/api/spellcheck/"
+    mainapiurl=mainapiurl.concat(word); 
+    fetch(mainapiurl,
+        {
+            mode:'no-cors',
+        })
+    .then(response=>response.json())
+    .then(content=>[
+        console.log(content)
+    ])
+    .catch(err=>{
+        console.log("ahahaha")
+    })
+}
+
+function giphy(androidsearchterm) {
+    // langcheck(androidsearchterm)
     document.getElementById("out").innerHTML = ""
-    const { search } = this.elements;
-    var s=search.value
+    document.getElementById("out").setAttribute("style","width:393px");
+    var s=androidsearchterm
     var author="@signwithrobert"
     var search_term=author.concat(" ",s)
     let apikey="DsCHXY2CPBGbdeWb6dJ1QFF1W0GWDr8A";
     let url=`https://api.giphy.com/v1/gifs/search?api_key=${apikey}&limit=1&q=`
     url=url.concat(search_term)
-    console.log(url)
     fetch(url)
     .then(response => response.json())
     .then(content=>{
@@ -17,17 +31,15 @@ form.addEventListener("submit", function(event) {
         let img = document.createElement("img");
         let fc = document.createElement("figcaption");
         img.src = content.data[0].images.downsized.url;
-        // img.alt = content.data[0].title;
-        // fc.textContent = content.data[0].title;
-        img.style.width='100px';
+        img.style.width='340px';
+        img.style.height='400px';
         fig.appendChild(img);
-        // fig.appendChild(fc);
         let out = document.querySelector(".out");
         out.insertAdjacentElement("afterbegin", fig);
-        document.querySelector("#search").value = "";
     })
     .catch(err=>{
         document.getElementById("out").innerHTML = "<h2>Oops! We didn't find the sign for your search term</h2>"
     }
         )
-  });
+}
+spellcheck("thissss")
