ab44b2a0f08c469d3ec4ddb53605e501ab831c6b
Prajwal Mani <56246392+prajwalmani@users.noreply.github.com>
2021-07-15 00:36:17 +0530

    pop up menu bug and apk

---
+++ src/SLI/app/src/main/java/com/example/sli/learncongratsactivity.java
@@ -0,0 +1,52 @@
+package com.example.sli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class learncongratsactivity extends AppCompatActivity {
+    Button predictlinkbtn;
+    Button speaklinkbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learncongratsactivity);
+        predictlinkbtn = findViewById(R.id.predictlinkbtn);
+        speaklinkbtn=findViewById(R.id.sppeaklinkbtn);
+        predictlinkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openpredactiv();
+
+            }
+        });
+
+        speaklinkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openspeakactiv();
+
+            }
+        });
+
+
+    }
+
+    private void openpredactiv() {
+        Intent intent =new Intent(this,predictactivity.class);
+        startActivity(intent);
+    }
+
+    private void openspeakactiv() {
+        Intent intent =new Intent(this,speakactivity.class);
+        startActivity(intent);
+    }
+
+}
